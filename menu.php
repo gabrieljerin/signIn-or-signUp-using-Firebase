ab127814f99d2376a0aa601b747cd5e04ea77927
jerin <jerin@Jerin-Alex>
2017-09-26 20:33:49 +0530

    #ui changes

--- menu.php
+++ menu.php
@@ -21,9 +21,9 @@
           
           <!-------------------DropDown menu without hover--------------------->
        <ul class="nav navbar-nav navbar-right">
-           <li id="signIn" class="drp-cls"><a class="a-cls-hover" href="#" onclick="showLoginModal(this)"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign in</a></li>
-           <li id="signUp" class="drp-cls"><a class="a-cls-hover" href="#" onclick="showSignUpModal(this)"><i class="fa fa-user-plus" aria-hidden="true"></i>Sign up</a></li>
-           <li id="signOut" class="drp-cls" style="display: none;"><a class="a-cls-hover" href="#" onclick="signOut(this)"><i class="fa fa-sign-out" aria-hidden="true"></i>Sign out</a></li>
+           <li id="signIn" class="drp-cls"><a class="a-cls-hover remove-dotted-lines" href="#" onclick="showLoginModal(this)"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign in</a></li>
+           <li id="signUp" class="drp-cls"><a class="a-cls-hover remove-dotted-lines" href="#" onclick="showSignUpModal(this)"><i class="fa fa-user-plus" aria-hidden="true"></i>Sign up</a></li>
+           <li id="signOut" class="drp-cls" style="display: none;"><a class="a-cls-hover remove-dotted-lines" href="#" onclick="signOut(this)"><i class="fa fa-sign-out" aria-hidden="true"></i>Sign out</a></li>
        </ul>
           <!-------------------------------------------------------------------->
   </div>
